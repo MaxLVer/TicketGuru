f89f88f96e96082545609d6930f9c6427f8dd115
MaxLVer <165693503+MaxLVer@users.noreply.github.com>
2025-01-28 20:08:12 +0200

    Initial Publish
    Added maven project with dependencies
    - Spring Boot Dev Tools
    - Spring Web

---
+++ ticket-guru/src/test/java/com/melkeinkood/ticket_guru/TicketGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melkeinkood.ticket_guru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
