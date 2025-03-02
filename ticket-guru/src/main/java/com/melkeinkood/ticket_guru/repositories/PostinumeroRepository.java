cde9d7069006acf8061a123950dccc392abcf9bf
MiVitikk <mikko.vitikka@myy.haaga-helia.fi>
2025-03-02 15:09:29 +0200

    Postinumeroille repo

---
+++ ticket-guru/src/main/java/com/melkeinkood/ticket_guru/repositories/PostinumeroRepository.java
@@ -0,0 +1,13 @@
+package com.melkeinkood.ticket_guru.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.melkeinkood.ticket_guru.model.Postinumero;
+
+@Repository
+public interface PostinumeroRepository extends JpaRepository<Postinumero, String>{
+    List<Postinumero> findByPostinumero (String postinumero);
+
+}
