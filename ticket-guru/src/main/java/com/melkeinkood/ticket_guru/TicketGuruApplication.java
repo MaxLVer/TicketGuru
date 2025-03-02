8622075b97ac4658e965e149266a5c6e9dbd7157
Ossi Lukkarinen <ossi.lukkarinen@myy.haaga-helia.fi>
2025-03-02 15:01:58 +0200

    Lisatty dataksi postiosoite ja muokattu muut datan syötöt nykyiseen muotoon

--- ticket-guru/src/main/java/com/melkeinkood/ticket_guru/TicketGuruApplication.java
+++ ticket-guru/src/main/java/com/melkeinkood/ticket_guru/TicketGuruApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.melkeinkood.ticket_guru.model.Postinumero;
+import com.melkeinkood.ticket_guru.repositories.PostinumeroRepository;
 import com.melkeinkood.ticket_guru.model.Tapahtuma;
 import com.melkeinkood.ticket_guru.repositories.TapahtumaRepository;
 import com.melkeinkood.ticket_guru.model.Tapahtumapaikka;
@@ -23,25 +25,32 @@ public class TicketGuruApplication {
 	}
 
 	@Bean
-	public CommandLineRunner tapahtumaData(TapahtumaRepository tapahtumaRepository, TapahtumapaikkaRepository tapahtumapaikkaRepository){
+	public CommandLineRunner tapahtumaData(TapahtumaRepository tapahtumaRepository, TapahtumapaikkaRepository tapahtumapaikkaRepository, PostinumeroRepository postinumeroRepository){
 		
 		return (args) -> {
-			Tapahtumapaikka korjaamo = new Tapahtumapaikka("Töölönkatu 51 B", "Helsinki", "Kulttuuritehdas Korjaamo");
+			Postinumero p00250 = new Postinumero("00250", "Helsinki");
+			postinumeroRepository.save(p00250);
+
+			Tapahtumapaikka korjaamo = new Tapahtumapaikka("Töölönkatu 51 B", p00250, "Kulttuuritehdas Korjaamo", 1000);
 			tapahtumapaikkaRepository.save(korjaamo);
 
 			Tapahtuma tapahtuma1 = new Tapahtuma(
-				LocalDateTime.of(2025, 2, 28, 20, 0),
 				korjaamo,
-				"Rock-konsertti"
-
+				LocalDateTime.of(2025, 2, 28, 20, 0),
+				"Rock-konsertti",
+				"Iloinen rock-tapahtuma",
+				500,
+				500
 			);
 			tapahtumaRepository.save(tapahtuma1);	
 
 			Tapahtuma tapahtuma2 = (new Tapahtuma(
-				LocalDateTime.of(2025, 2, 27, 20, 0),
 				korjaamo,
-				"Pop-konsertti"
-
+				LocalDateTime.of(2025, 2, 27, 20, 0),
+				"Pop-konsertti",
+				"Hurja pop-tapahtuma",
+				200,
+				200
 			));
 			tapahtumaRepository.save(tapahtuma2);	
 
