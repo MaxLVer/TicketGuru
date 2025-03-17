7311f88ffd18ce1163d1bf58de82e8a36854ffdf
Liisa Davydov <liisamuttonen@gmail.com>
2025-03-17 06:38:29 +0200

    Korjaa repot palauttamaan single value instead of list

--- ticket-guru/src/main/java/com/melkeinkood/ticket_guru/repositories/AsiakastyyppiRepository.java
+++ ticket-guru/src/main/java/com/melkeinkood/ticket_guru/repositories/AsiakastyyppiRepository.java
@@ -1,6 +1,6 @@
 package com.melkeinkood.ticket_guru.repositories;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +11,5 @@ import com.melkeinkood.ticket_guru.model.Asiakastyyppi;
 @Repository
 public interface AsiakastyyppiRepository extends JpaRepository <Asiakastyyppi, Long>{
 
-    List<Asiakastyyppi> findByAsiakastyyppiId(Long AsiakastyyppiId);
+    Optional<Asiakastyyppi> findByAsiakastyyppiId(Long asiakastyyppiId);
 } 
