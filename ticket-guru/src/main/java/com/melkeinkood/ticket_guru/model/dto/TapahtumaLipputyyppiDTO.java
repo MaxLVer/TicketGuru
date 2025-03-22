020077e8eec8cdf0fc2b4d3b9f83aded08326721
Liisa Davydov <liisamuttonen@gmail.com>
2025-03-22 09:48:48 +0200

    Muokattu DTO:ta sisältämään href linkit

--- ticket-guru/src/main/java/com/melkeinkood/ticket_guru/model/dto/TapahtumaLipputyyppiDTO.java
+++ ticket-guru/src/main/java/com/melkeinkood/ticket_guru/model/dto/TapahtumaLipputyyppiDTO.java
@@ -2,26 +2,28 @@ package com.melkeinkood.ticket_guru.model.dto;
 
 import java.math.BigDecimal;
 
-import org.springframework.hateoas.RepresentationModel;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class TapahtumaLipputyyppiDTO extends RepresentationModel<TapahtumaLipputyyppiDTO> {
+public class TapahtumaLipputyyppiDTO {
 
-    private long tapahtumaLipputyyppiId;
-    private long tapahtumaId;
-    private long asiakastyyppiId;
+    private Long tapahtumaLipputyyppiId;
+    private Long tapahtumaId; 
+    private String tapahtumaHref; 
+    private Long asiakastyyppiId; 
+    private String asiakastyyppiHref; 
     private BigDecimal hinta;
 
-    public TapahtumaLipputyyppiDTO() {}
-
-    public TapahtumaLipputyyppiDTO(long tapahtumaLipputyyppiId, long tapahtumaId, long asiakastyyppiId, BigDecimal hinta) {
+    public TapahtumaLipputyyppiDTO(Long tapahtumaLipputyyppiId, Long tapahtumaId, String tapahtumaHref, Long asiakastyyppiId, String asiakastyyppiHref, BigDecimal hinta) {
         this.tapahtumaLipputyyppiId = tapahtumaLipputyyppiId;
         this.tapahtumaId = tapahtumaId;
+        this.tapahtumaHref = tapahtumaHref;
         this.asiakastyyppiId = asiakastyyppiId;
+        this.asiakastyyppiHref = asiakastyyppiHref;
         this.hinta = hinta;
     }
+
 }
