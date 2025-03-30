617af2a56e983aab4af39daffc982f2f160fa5ab
Ossi Lukkarinen <ossi.lukkarinen@myy.haaga-helia.fi>
2025-03-30 12:40:06 +0300

    AutentikointiControlleri tehty

---
+++ ticket-guru/src/main/java/com/melkeinkood/ticket_guru/web/AutentikointiController.java
@@ -0,0 +1,30 @@
+package com.melkeinkood.ticket_guru.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import com.melkeinkood.ticket_guru.auth.services.JwtService;
+
+
+@RestController
+public class AutentikointiController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping("/autentikointi")
+    public String autentikointi(@RequestParam("kayttajanimi") String kayttajanimi,
+            @RequestParam("salasana") String salasana) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(kayttajanimi, salasana));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return jwtService.generateToken(kayttajanimi);
+    }
+}
