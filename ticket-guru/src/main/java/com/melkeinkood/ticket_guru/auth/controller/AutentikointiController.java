cab16cf03b71266f9c76d45a7d38dc512c3f4829
Liisa Davydov <liisamuttonen@gmail.com>
2025-03-31 21:27:32 +0300

    test

--- ticket-guru/src/main/java/com/melkeinkood/ticket_guru/auth/controller/AutentikointiController.java
+++ ticket-guru/src/main/java/com/melkeinkood/ticket_guru/auth/controller/AutentikointiController.java
@@ -1,5 +1,5 @@
 package com.melkeinkood.ticket_guru.auth.controller;
-/* 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -8,6 +8,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import com.melkeinkood.ticket_guru.auth.services.JwtService;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -20,12 +22,11 @@ public class AutentikointiController {
     private JwtService jwtService;
 
     @PostMapping("/autentikointi")
-    public String autentikointi(@RequestParam("kayttajanimi") String kayttajanimi,
-            @RequestParam("salasana") String salasana) {
+    public String autentikointi(@Valid @RequestBody String kayttajanimi,
+    @Valid @RequestBody String salasana) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(kayttajanimi, salasana));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return jwtService.generateToken(kayttajanimi);
     }
 }
-*/
--- ticket-guru/src/main/java/com/melkeinkood/ticket_guru/auth/services/JwtService.java
+++ ticket-guru/src/main/java/com/melkeinkood/ticket_guru/auth/services/JwtService.java
@@ -1,64 +1,61 @@
 package com.melkeinkood.ticket_guru.auth.services;
-// package com.melkeinkood.ticket_guru.service;
 
-// import java.security.Key;
-// import java.util.Date;
+import java.security.Key;
+import java.util.Date;
 
-// import javax.crypto.SecretKey;
+import javax.crypto.SecretKey;
 
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-// import io.jsonwebtoken.*;
-// import io.jsonwebtoken.io.Decoders;
-// import io.jsonwebtoken.security.Keys;
-// import io.jsonwebtoken.security.SignatureException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 
-// @Component
-// public class JwtService {
-//     @Value("${jwt.sercet}")
-//     private String secretKey;
+@Component
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secretKey;
 
-//     @Value("${jwt.expiration}")
-//     private Long jwtExpirationInMS;
+    @Value("${jwt.expiration}")
+    private Long jwtExpirationInMS;
 
-//     private Key getSingInKey(){
-//         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-//         return Keys.hmacShaKeyFor(keyBytes);
-//     }
+    private Key getSingInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
-//     private SecretKey key =  Jwts.SIG.HS256.key().build();
+    private SecretKey key =  Jwts.SIG.HS256.key().build();
 
+    public String generateToken(String kayttajanimi) {
+        Date now = new Date ();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMS);
+        return Jwts.builder()
+                .subject(kayttajanimi)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSingInKey())
+                .compact();
+    }
     
+    public String getUsernameFromToken (String token){
+        Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.getSubject();
 
-//     public String generateToken(String kayttajanimi) {
-//         Date now = new Date ();
-//         Date expiryDate = new Date(now.getTime() + jwtExpirationInMS);
-//         return Jwts.builder()
-//                 .subject(kayttajanimi)
-//                 .issuedAt(now)
-//                 .expiration(expiryDate)
-//                 .signWith(getSingInKey())
-//                 .compact();
-//     }
-    
-//     public String getUsernameFromToken (String token){
-//         Claims claims = Jwts.parser()
-//                 .verifyWith(key)
-//                 .build()
-//                 .parseSignedClaims(token)
-//                 .getPayload();
-//         return claims.getSubject();
+    }
 
-//     }
-
-//     public boolean validateToken(String token) {
-//         try {
-//             Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
-//             return true;
-//         } catch (SignatureException | MalformedJwtException | ExpiredJwtException | UnsupportedJwtException | IllegalArgumentException ex){
-//             System.out.println("Invalid token");
-//         }
-//         return false;
-//     }
-// }
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+            return true;
+        } catch (SignatureException | MalformedJwtException | ExpiredJwtException | UnsupportedJwtException | IllegalArgumentException ex){
+            System.out.println("Invalid token");
+        }
+        return false;
+    }
+}
