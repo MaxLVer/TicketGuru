27466c7bccc124cd6f062c95d8ebe62858cdc4be
Liisa Davydov <122706955+davylis@users.noreply.github.com>
2025-03-31 10:37:30 +0300

    Update CustomUserDetailsService.java

--- ticket-guru/src/main/java/com/melkeinkood/ticket_guru/auth/services/CustomUserDetailsService.java
+++ ticket-guru/src/main/java/com/melkeinkood/ticket_guru/auth/services/CustomUserDetailsService.java
@@ -1,33 +1,33 @@
-package com.melkeinkood.ticket_guru.auth.services;
+// package com.melkeinkood.ticket_guru.auth.services;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
+// import org.springframework.stereotype.Service;
 
-import com.melkeinkood.ticket_guru.repositories.KayttajaRepository;
-import com.melkeinkood.ticket_guru.model.Kayttaja;
+// import com.melkeinkood.ticket_guru.repositories.KayttajaRepository;
+// import com.melkeinkood.ticket_guru.model.Kayttaja;
 
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-    @Autowired
-    private KayttajaRepository kayttajaRepository;
+// @Service
+// public class CustomUserDetailsService implements UserDetailsService {
+//     @Autowired
+//     private KayttajaRepository kayttajaRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String kayttajanimi) throws UsernameNotFoundException {
-        Kayttaja kayttaja = kayttajaRepository.findByKayttajanimi(kayttajanimi);
+//     @Override
+//     public UserDetails loadUserByUsername(String kayttajanimi) throws UsernameNotFoundException {
+//         Kayttaja kayttaja = kayttajaRepository.findByKayttajanimi(kayttajanimi);
 
-        if (kayttaja == null) {
-            throw new UsernameNotFoundException("Käyttäjää ei löydy: " + kayttajanimi);
-        }
+//         if (kayttaja == null) {
+//             throw new UsernameNotFoundException("Käyttäjää ei löydy: " + kayttajanimi);
+//         }
 
-        return new org.springframework.security.core.userdetails.User(
-        kayttaja.getKayttajanimi(),
-        kayttaja.getSalasana(),
-        new ArrayList<>()
-    );
-}
-}
+//         return new org.springframework.security.core.userdetails.User(
+//         kayttaja.getKayttajanimi(),
+//         kayttaja.getSalasana(),
+//         new ArrayList<>()
+//     );
+// }
+// }
